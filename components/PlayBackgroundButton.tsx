ab9af4caa716af772df9847006822cd391fa402e
cs-dev-port <aidreamlab@gmail.com>
2026-08-15 23:50:59 -0400

    please

--- components/PlayBackgroundButton.tsx
+++ components/PlayBackgroundButton.tsx
@@ -1,16 +1,31 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+const UNLOCK_KEY = "outer-bg-unlocked";
 
 export default function PlayBackgroundButton({ src }: { src: string }) {
   const videoRef = useRef<HTMLVideoElement>(null);
   const [started, setStarted] = useState(false);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (!video) return;
+
+    if (sessionStorage.getItem(UNLOCK_KEY) === "true") {
+      video
+        .play()
+        .then(() => setStarted(true))
+        .catch(() => {});
+    }
+  }, []);
 
   const handlePlay = () => {
     const video = videoRef.current;
     if (!video) return;
     video.play().catch(() => {});
     setStarted(true);
+    sessionStorage.setItem(UNLOCK_KEY, "true");
   };
 
   return (
